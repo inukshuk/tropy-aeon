4ae5c995a99edcdecd4f8912d46a7676c9938756
Sylvester Keil <sylvester@keil.or.at>
2018-06-14 16:00:24 +0200

    Fix default options
    Add header option

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
-const fs = require('fs')
+const { createWriteStream } = require('fs')
+const { remote } = require('electron')
+const { join } = require('path')
 
 class AeonPlugin {
   constructor(options, context) {
@@ -16,23 +18,28 @@ class AeonPlugin {
 
   async export(data) {
     let path = await this.dialog.save({
-      defaultPath: this.options.file
+      defaultPath: this.defaultPath
     })
 
     if (!path) return
 
     this.logger.info('Exporting items to Aeon Timeline...')
-    let ws = fs.createWriteStream
+    let ws = createWriteStream(path, {
+      autoclose: true,
+      flags: 'w'
+    })
+
+    if (this.options.header) {
+      ws.write(`${this.header}\n`)
+    }
 
     for (let items of data) {
-      for (let item of data['@graph']) {
+      for (let item of items['@graph']) {
         try {
           ws.write(`${[
             this.encode(item.title),
             this.encode(item.date),
-            this.encode([
-              item.photos.map(p => p.path).join('|')
-            ])
+            this.encode(item.photo.map(p => p.path).join('|'))
           ].join(',')}\n`)
 
         } catch (e) {
@@ -48,9 +55,28 @@ class AeonPlugin {
     return this.context.require('../dialog')
   }
 
+  get defaultPath() {
+    return join(
+      remote.app.getPath('home'),
+      this.options.file
+    )
+  }
+
+  get header() {
+    return this.options.quotes ?
+      '"Title","Start","Links"' :
+      'Title,Start,Links'
+  }
+
   get logger() {
     return this.context.logger
   }
+}
+
+AeonPlugin.defaults = {
+  file: 'tropy-aeon.csv',
+  header: false,
+  quotes: true
 }
 
 module.exports = AeonPlugin
